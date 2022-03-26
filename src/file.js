4e149c571356b562c879c93ec5e7ccb7b840da1a
SamStarlling <samelabarbosa.06@gmail.com>
2022-03-26 10:57:05 -0300

    feat: The new getFileContent method takes the path of the file

---
+++ src/file.js
@@ -0,0 +1,30 @@
+const { readFile } = require('fs/promises');
+const { join } = require('path');
+
+//Object with validations
+const DEFAULT_OPTIONS = {
+  maxLines: 3,
+  fields: [ 'id', 'name', 'profession', 'age' ]
+};
+
+class File {
+  static async csvJson(filePath) {
+    const content = await File.getFileContent(filePath);
+    return content;
+  }
+
+  //Return content about this filePath
+  static async getFileContent(filePath) {
+    //Get file path
+    const fileName = join(__dirname, filePath);
+
+    //Convert file content to brazilian format
+    return ( await readFile(fileName)).toString('utf8');
+  }
+}
+
+//clojure (auto excute function - IFE) - call file methods
+(async () => {
+  const result = await File.csvJson('./../mocks/three-items-valid.csv');
+  console.log('result', result);
+})();
