2e2ca4265579d7d2f5ff9e0c11d0362f4ca2777b
SamStarlling <samelabarbosa.06@gmail.com>
2022-03-27 10:52:07 -0300

    test: Create test for unhappy scenarios

--- index.test.js
+++ index.test.js
@@ -4,9 +4,28 @@ const { rejects } = require('assert');
 
 //clojure (auto excute function - IFE) - call file methods
 (async () => {
-  const filePath = './mocks/empty-file-invalid.csv';
-  const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
-  const result = File.csvToJson(filePath);
+  //Test when the file is empty
+  {
+    const filePath = './mocks/empty-file-invalid.csv';
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
+    await rejects(result, rejection);
+  };
+  
+  //Test when the file has invalid length
+  {
+    const filePath = './mocks/four-items-invalid.csv';
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
+    await rejects(result, rejection);
+  };
+  
+  //Test when the file has invalid properties
+  {
+    const filePath = './mocks/four-items-invalid.csv';
+    const rejection = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
+    await rejects(result, rejection);
+  };
 
-  await rejects(result, rejection);
 })();
