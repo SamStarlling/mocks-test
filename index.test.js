6abda178e682776fc5c7824d81f70ee0831ff5ee
SamStarlling <samelabarbosa.06@gmail.com>
2022-04-12 20:35:03 -0300

    test: Add test for valid CSV file

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 const { error } = require('./src/constants');
 const File = require('./src/file');
-const { rejects } = require('assert');
+const { rejects, deepStrictEqual } = require('assert');
 
 //clojure (auto excute function - IFE) - call file methods
 (async () => {
@@ -10,7 +10,7 @@ const { rejects } = require('assert');
     const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
     const result = File.csvToJson(filePath);
     await rejects(result, rejection);
-  };
+  }
   
   //Test when the file has invalid length
   {
@@ -18,14 +18,34 @@ const { rejects } = require('assert');
     const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
     const result = File.csvToJson(filePath);
     await rejects(result, rejection);
-  };
+  }
   
-  //Test when the file has invalid properties
+  //Test when the file is valid
   {
-    const filePath = './mocks/four-items-invalid.csv';
-    const rejection = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
-    const result = File.csvToJson(filePath);
-    await rejects(result, rejection);
-  };
+    const filePath = './mocks/three-items-valid.csv';
+    const result = await File.csvToJson(filePath);
+    const expected = [
+      {
+        id: 123,
+        name: 'Erick Wendel',
+        profession: 'Javascript Instructor',
+        age: 25
+      },
+      {
+        id: 321,
+        name: 'Xuxa da Silva',
+        profession: 'Javascript Specialist',
+        age: 80
+      },
+      {
+        id: 231,
+        name: 'Joaozinho',
+        profession: 'Java Developer',
+        age: 30
+      }
+    ];
+    //Verify value and reference
+    deepStrictEqual(result, expected);
+  }
 
 })();
