6e4a595a482c60b201e805e6417514b2e665e2c6
SamStarlling <samelabarbosa.06@gmail.com>
2022-04-13 22:33:47 -0300

    feat: Create the Service class with makeRequest method

---
+++ stubs-class/src/service.js
@@ -0,0 +1,17 @@
+const https = require('https');
+
+class Service {
+  async makeRequest(url) {
+    return new Promise((resolve, reject) => {
+      https.get(url, response => {
+        response.on('data', (data) => resolve(data));
+        response.on('error', reject);
+      });
+    });
+  };
+};
+
+(async () => {
+  const service = await new Service().makeRequest('https://swapi.dev/api/planets/1/');
+  console.log('response', service);
+})()
