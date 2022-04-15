11e1c7faa847b42b1df0551ecbfab5b32e1a8521
SamStarlling <samelabarbosa.06@gmail.com>
2022-04-15 00:19:40 -0300

    test:  Makes the BASE_URL_1 request, gets the data to generate the mock file

--- stubs-class/src/service.test.js
+++ stubs-class/src/service.test.js
@@ -4,7 +4,7 @@ const BASE_URL_2 = 'https://swapi.dev/api/planets/2/';
 (async () => {
   {
     const service = new Service();
-    const withoutStub = await service.makeRequest(BASE_URL_2);
+    const withoutStub = await service.makeRequest(BASE_URL_1);
     console.log(JSON.stringify(withoutStub));
   }
 })()
