060d8471158732fa64bfd49cf634dfc6bdd7dfe7
SamStarlling <samelabarbosa.06@gmail.com>
2022-04-16 11:33:14 -0300

    refactor: Remove the test from this file and refactor makeRequest method syntax

--- stubs-class/src/service.js
+++ stubs-class/src/service.js
@@ -4,16 +4,11 @@ class Service {
   async makeRequest(url) {
     return new Promise((resolve, reject) => {
       https.get(url, response => {
-        response.on('data', (data) => resolve(JSON.parse(data)));
+        response.on('data', data => resolve(JSON.parse(data)));
         response.on('error', reject);
       });
     });
   };
 };
-
-(async () => {
-  const service = await new Service().makeRequest('https://swapi.dev/api/planets/1/');
-  console.log('response', service);
-})()
 
 module.exports = Service;
